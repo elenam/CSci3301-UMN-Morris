be77d7c03f89a8069401fb0af06b848c6f9f1ef2
Elena M <elenam@morris.umn.edu>
2024-01-31 07:48:42 -0600

    added @FunctionalInterface for Observable

--- Head-First-Design-Patterns-master/src/headfirst/designpatterns/observer/weather/Observer.java
+++ Head-First-Design-Patterns-master/src/headfirst/designpatterns/observer/weather/Observer.java
@@ -1,5 +1,8 @@
 package headfirst.designpatterns.observer.weather;
 
+@FunctionalInterface
+// Observer is a functional interface since it has only one method
+// to implement
 public interface Observer {
 	public void update(float temp, float humidity, float pressure);
 }
