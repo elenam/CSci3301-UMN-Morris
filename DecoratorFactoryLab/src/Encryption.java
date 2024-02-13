5a7f5f1f9d0029658a21a3e26e9b6acfb3059b48
Elena M <elenam@morris.umn.edu>
2024-02-13 09:57:51 -0600

    added a note about closing the stream

--- DecoratorFactoryLab/src/Encryption.java
+++ DecoratorFactoryLab/src/Encryption.java
@@ -19,6 +19,8 @@ public class Encryption {
 			//TODO: add EncryptionOutputStream to decorate the FileOutputStream
 			// before passing it to the OutputStreamWriter:
 			// out1 = .....
+			// Note: if you get an error about a resource leak or non-closing 
+			// stream, close all of the streams in the 'finally' block
 			out = new OutputStreamWriter(out1, "UTF-8");
 			out.write(s); // write to the file
 		} catch (IOException e) {
