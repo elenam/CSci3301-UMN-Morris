6ba135490cd03d3103ff27fc5fe295b0e54b34f8
Elena M <elenam@morris.umn.edu>
2024-02-20 12:07:40 -0600

    added an example of adding a field to enum

--- Head-First-Design-Patterns-master/src/headfirst/designpatterns/singleton/enumS/Singleton.java
+++ Head-First-Design-Patterns-master/src/headfirst/designpatterns/singleton/enumS/Singleton.java
@@ -2,11 +2,21 @@ package headfirst.designpatterns.singleton.enumS;
 
 public enum Singleton {
 	UNIQUE_INSTANCE;
- 
 	// other useful fields here
+	
+	// I added an example of a field and checked setting it -Elena
+	private boolean empty;
 
 	// other useful methods here
 	public String getDescription() {
 		return "I'm a thread safe Singleton!";
+	}
+	
+	public void setEmpty(boolean b) {
+		empty = b;
+	}
+	
+	public boolean getEmpty() {
+		return empty;
 	}
 }
--- Head-First-Design-Patterns-master/src/headfirst/designpatterns/singleton/enumS/SingletonClient.java
+++ Head-First-Design-Patterns-master/src/headfirst/designpatterns/singleton/enumS/SingletonClient.java
@@ -4,5 +4,9 @@ public class SingletonClient {
 	public static void main(String[] args) {
 		Singleton singleton = Singleton.UNIQUE_INSTANCE;
 		System.out.println(singleton.getDescription());
+		
+		singleton.setEmpty(true);
+		
+		System.out.println("empty = " + singleton.getEmpty());
 	}
 }
